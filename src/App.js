c9421cb3abb497efd672f9e196eb6cbc1dd0c376
a-sep <artur.sep@gmail.com>
2017-10-09 06:40:47 +0200

    connected to firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
+import * as firebase from 'firebase'
+
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: {
+        id: '',
+        name: '',
+        age: '',
+        country: '',
+      }
+    }
+  }
+
+  componentDidMount() {
+    const usersListRef = firebase.database().ref().child('usersList')
+    const userRef = usersListRef.child('user')
+    userRef.on('value', snap => {
+      console.log(snap.val())
+      this.setState({
+        user: snap.val(),
+      })
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -17,6 +42,12 @@ class App extends Component {
           Each component should have separate css file with identical name in the same folder (ex. NewComponent.js have NewComponent.css).
           App.js is a body of our clone, here we import and use (render) new components.
         </p>
+
+        <h2>test user from firebase:</h2>
+          <p>id: {this.state.user.id}</p>
+          <p>Name: {this.state.user.name}</p>
+          <p>Age: {this.state.user.age}</p>
+          <p>Country: {this.state.user.country}</p>
       </div>
     );
   }
