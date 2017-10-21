e45f214fb4c98a15d2a72b7e1c1f2ce069002107
a-sep <artur.sep@gmail.com>
2017-10-21 12:47:21 +0200

    added values from .env to client credentials

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      usersList: [],
     };
   }
 
@@ -17,23 +16,11 @@ class App extends Component {
     });
   }
 
-
   componentWillUnmount() {
     base.removeBinding(this.usersListRef);
   }
 
   render() {
-    const usersList = this.state.usersList.map(user => (
-      <div key={user.name}>
-        <h3>{user.name}</h3>
-        <ul style={{ listStyle: 'none' }}>
-          <li>id: {user.id}</li>
-          <li>age: {user.age}</li>
-          <li>country: {user.country}</li>
-        </ul>
-      </div>
-    ));
-
     return (
       <div className="App">
         <header className="App-header">
@@ -42,9 +29,6 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-
-        <h3>Users in firebase:</h3>
-        {usersList}
       </div>
     );
   }
