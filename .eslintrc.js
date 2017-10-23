fcdbca5f03d7a5e2bfa033e6379f5962eea350b6
a-sep <artur.sep@gmail.com>
2017-10-23 04:58:03 +0200

    installed material-ui

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,6 @@
 module.exports = {
     "extends": "airbnb",
-    "plugins": [
-        "react",
-        "jsx-a11y",
-        "import"
-    ],
     "rules": {
         "react/jsx-filename-extension": 0,
-        "jsx-a11y/label-has-for": 0
       }
 };
