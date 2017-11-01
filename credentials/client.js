ee69b504ab31acc66da8c949eb4f99c872b1480a
a-sep <artur.sep@gmail.com>
2017-11-01 18:12:59 +0100

    .gitignore updated

--- credentials/client.js
+++ credentials/client.js
@@ -1,9 +1,9 @@
 // add your data from firebase here
 module.exports = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
+  apiKey: 'AIzaSyDPYe5gUQYWo6oupbpBHFLElaCrYNcprzg',
+  authDomain: 'voyage2-bears18.firebaseapp.com',
+  databaseURL: 'https://voyage2-bears18.firebaseio.com',
+  projectId: 'voyage2-bears18',
+  storageBucket: 'voyage2-bears18.appspot.com',
+  messagingSenderId: '290913994215',
 };
