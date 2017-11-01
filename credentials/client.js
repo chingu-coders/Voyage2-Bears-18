cb5d5393c11db43e8e01b526a74f9fba86b0ec95
a-sep <artur.sep@gmail.com>
2017-11-01 20:08:13 +0100

    added dotenv to hide client credensials

--- credentials/client.js
+++ credentials/client.js
@@ -1,9 +1,8 @@
-// add your data from firebase here
 module.exports = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
+  apiKey: process.env.FIREBASE_KEY,
+  authDomain: process.env.FIREBASE_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_SENDER_ID,
 };
--- credentials/server.js
+++ credentials/server.js
@@ -1,13 +1,27 @@
-// add your data from firebase here
+// module.exports = {
+//   type: process.env.FIREBASE_SERVER_TYPE,
+//   project_id: process.env.FIREBASE_SERVER_PROJECT_ID,
+//   private_key_id: process.env.FIREBASE_SERVER_PRIVATE_KEY_ID,
+//   private_key: process.env.FIREBASE_SERVER_PRIVATE_KEY,
+//   client_email: process.env.FIREBASE_SERVER_CLIENT_EMAIL,
+//   client_id: process.env.FIREBASE_SERVER_CLIENT_ID,
+//   auth_uri: process.env.FIREBASE_SERVER_AUTH_URI,
+//   token_uri: process.env.FIREBASE_SERVER_TOKEN_URI,
+//   auth_provider_x509_cert_url: process.env.FIREBASE_SERVER_AUTH_PROVIDER_X509_CERT_URL,
+//   client_x509_cert_url: process.env.FIREBASE_SERVER_CLIENT_X509_CERT_URL,
+// };
+
 module.exports = {
-  type: '',
-  project_id: '',
-  private_key_id: '',
-  private_key: '',
-  client_email: '',
-  client_id: '',
-  auth_uri: '',
-  token_uri: '',
-  auth_provider_x509_cert_url: '',
-  client_x509_cert_url: '',
+  type: 'service_account',
+  project_id: 'voyage2-bears18',
+  private_key_id: 'f18768fce4cec0c41175bc146397188ec7c45490',
+  private_key:
+    '-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDTLC/sWlXlHBte\n14IXjYYgqX4UUZWitUlYn2Z3fKgXqHf5F5vlzwIhNy6UUMQVX2m+Pz5zVts/znKk\nb6mKCp6zu3PhDlXCJNZXK3e9zgPS8xXA3gXhDZ9flofY5F1cqUVGTlhQYowz9P3E\nCyEi0gYt/YdmJKdW6fDti773uAo9+0W2InlAVylm7C76EQBOwGla0eKenqKtFS8j\nG4dJHFg/T3oKFbXHripEjIKxl8RRNL5V028TxbRXGnfN0xKZVpbNZE6pGsUud6dR\n8+wSrtIzNlCiT09XZygby8E/GTdSHVw/ROT84h05gE/Z5KRXg3nOA6SoFqEvfVEC\nyYI4T8LhAgMBAAECggEATTfYtFAaHaOn/IfIec1V8QaZZFuq4GrzDZWIFgQ+wRvo\n9uxjpOaL7FJt1W0tLnWLyLX+o5VsqcnXkPqF38TfYHCYwICJ6mqm+AwchorZSh96\nYmKteqyNDRqRxUVIQHRjMQC/+XD3vJ4G11OMg9uRx+pvwvF/QWBdKbfSlmm1UREx\nNKxMcnIrPSCYjfTfYPYAp8PLkAcVjXn4I+KDEqpZFzHtMM/3vkuknX0iPlAyshcj\n8z5qIBviRxUhe5dxynSLrn8EscMqObUe3NBm/5fCspDgiUfS25DI13ee3NNLP3ec\nOu8DJv5jfjSACyUeNYFQei5mTIhgRiLLIuhuWBHU+wKBgQDxYt2M/gGyCm5X0bst\noCBi4Pf+Pn/oOnDBb0boq3IIBZe35zcyPqrMob9YAE9AZjycXB0AQ7DJUdtcsE0d\n17Sn9zmvxlYqWkqONhEExSH7MxShBgtL7O4Z4kj73daQ0d+iHHU28xRcoarZq6yg\nnzHce3iQqTH/W6ymNwRuUxOIawKBgQDf9Q4e2/6cSDZxNtZkwYdsHeb++Ktgz4KV\n0RJ6FHdCVnmOOOJSTr3S6BtLvUJJjnM+DNoJ9vicWC+oiyGqPI61WTNqZlyJTNzs\n59Ptv/ZDE69fLjCPILQdVRJuP7tUVtvXbqNeOAVe6AF9+4ddjuhwxBKQZin6vMnP\nu9osQxxk4wKBgDGYJ37lXUHQRNWEQNnh8WV/ZVR2UN9MyofjrgT13KrrQz3EoqGP\nIJaEkQSqo53Oo5tYjZjVT4g9aj2D94QvIm30LIUB0ghHntPTJfH98rpix1VCIaYK\n2uy0glxFOuVsSW9MO4T7sm5JxQmN7RzfOGZdc+r86EnX8wP/f8iPBw9bAoGADJwV\noXMtkMsYFaPKyn2DDHlaijyiGZ9VcO4Gpk0Esf2AdpuJUq0QTxG2h7suQkn8/x9m\nH22CbfshI9JBoP4r4tYU6Qa+axSi08vxyM8oJHq1Qxysq0JiIcyqbv6zo3oIgsa9\n+A4bb06M1vUUmBX1zHOdv/egoXeDvWiMTjWxIZUCgYEAz1MgMa9SoTEaIHMlQEMH\nhjijoUXjUksldo3fd3UKF/aiUE8hB0KF22hdaxvOh5Hluliq6acOOjERBIw28pRJ\n6D/EUuMDFPlHM/tf0YEhUHp+VkJ3OnHhIb4NihiqYdJn3vwJ2F7DklGBx4djVuxf\nvjkL4kZZXbgU3Lfjp5aNyqU=\n-----END PRIVATE KEY-----\n',
+  client_email: 'firebase-adminsdk-kd6h6@voyage2-bears18.iam.gserviceaccount.com',
+  client_id: '107511881666754577444',
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://accounts.google.com/o/oauth2/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url:
+    'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-kd6h6%40voyage2-bears18.iam.gserviceaccount.com',
 };
