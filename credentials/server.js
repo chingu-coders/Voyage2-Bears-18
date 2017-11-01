84be9978e0f9365bbcbcc0c154f8d9636d8b09d1
a-sep <artur.sep@gmail.com>
2017-11-01 18:18:10 +0100

    update

--- credentials/server.js
+++ credentials/server.js
@@ -1,15 +1,12 @@
-// add your data from firebase here
 module.exports = {
-  type: 'service_account',
-  project_id: 'voyage2-bears18',
-  private_key_id: 'd6f95615fda85af5af6322c0be0e5cfa46f5835e',
-  private_key:
-    '-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCxyfZXL7FCl7w0\nfQ/RQJ4Wxc/9PPnWdLrM3C54VFILGBEEWbddfeR64eVDXe15p8rOklfFFVQoZHI3\nxpqiLR8PlLMzd+QICOovaFKXmWhOFouvPLCXnJyZtuaNvhjjtdV09NhX8oXLq0M7\nOAQidsnvGE8amqMUiubiz6hj7aBSqsk5Wu/kkmIxXRYamyt5DnlX7YEqKcCtdErK\nNMEEYvTPTe+BD+1YzzOU/Y/mgyng2gZlNNSCVkMmAYZMyb71EfWM0yNxU64rUECM\nZXJE1AUup0wUbocLQ4GJI3rOHGB8Dsc7gVt5ohrapmSL3OuuBeknZ3KslA320eUl\noGpO8QwJAgMBAAECggEAAeZdmoro7a7qFPQtLMLwWDVJnAmAcKPoschnOrlJSH27\nric4nWgl/pDWvq83RXCJ60aHEj/Abs9mpQ/wlmu9ruXw5aDnWpVLI1yNsrmx8qZX\nscoH57wwXuLPIz6fzMx5o86mAofrRBo9aq7rjw65Pl6Cdzn7JNIN1j36glmHYrq/\nM3W742RTC8LhZLToRsEDEf8nOBNZ2K1a8EkldZ4By41cSFjZjGWIxeAWEEg3SedP\nIvd5DSRzFjYHt3+tgnwTESO37AFwOryDbUpM+GyAgY7g23iPSDx4R1ZkcKK74kyv\nS50rNmrghtIchIg18FXVYx6tz1pu2MYE9OmIVu9QnQKBgQD6au443X0vBujP0ro8\nZjYEb/8uBZ6I8ohflSgQmGq4yT3z+dtekiBji6bexiqpy4qxWQeNbLImtJOTeFSE\nKkWjee6TiJbl+YQe2yuTapjH1ZSMen7eMNwzXLm113YzHC2xCQooofWycOV39Egv\nnBOj/VC2I4JCMfPFPOPPfD74EwKBgQC1wI7TnZuPAcjHltjl7+RXDOQCwxoM86Dh\nOaYQxGG5kKrJzLs8j7MjG6rJ2TT0xMtbhsaN5XNJ5OXAEffYpWvcK3Zj5scsxqqQ\nZqIyqXEfG6TQC3d8sodI4faVbtkptPbNOT3AF5w5ANqhyWTMcRHuD5+3xYndHhvB\ngL6ViNxm8wKBgBoivupAc6hM2E+YdseHA2xDJCbxfzJb3c/cCWpx/fF7T3P/R93+\n8lJxaOEnXZyLGdaYrCkehxlzD1P84xVINCxztWrAfr9R43A0/79LCNcSe4aN4l/e\nSZ0yuE86RaPf9QTdJKpI/RO6pLh4TnBpocMj1SCtZBL0k7SqPjyRgyDxAoGADCkr\nJUuZWfMznO8h6hin/Y8WPn0RZCZ08DzsOJj7NPBSDRiOupme0c2LGuBWo737IU+e\nP2ZH1XzkRnqtS6id0+hwjnSlhQBx7yF+3sbpk7xExcrOnuBPHvhHiG4XR6wdC1Q2\nJoEt6b594m9YODmkinovJJuqAsp97u+PUPAMVzMCgYEAppIXuYVJzGS47K4p+aCq\naZ/NnhF/OsLdw2xWOrrq2n3DJKpg6ukqtfz8HYsffMsxoAY/k2YLKGqaqJ05NKfA\nVJ3S1KKC/H6YW5qMFaDmREt+5gGJbvf9tlcNtw8XlaaJFEIbjtWzAX5by+hrmr2b\ncP6aL6+64UJhBBTDUc6OsE4=\n-----END PRIVATE KEY-----\n',
-  client_email: 'firebase-adminsdk-kd6h6@voyage2-bears18.iam.gserviceaccount.com',
-  client_id: '107511881666754577444',
-  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-  token_uri: 'https://accounts.google.com/o/oauth2/token',
-  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-  client_x509_cert_url:
-    'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-kd6h6%40voyage2-bears18.iam.gserviceaccount.com',
+  type: '',
+  project_id: '',
+  private_key_id: '',
+  private_key: '',
+  client_email: '',
+  client_id: '',
+  auth_uri: '',
+  token_uri: '',
+  auth_provider_x509_cert_url: '',
+  client_x509_cert_url: '',
 };
